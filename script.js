f509ef412f87c5f06d3e4f95c7304b6cbe4f7880
Jtrinity87 <mercado.jacqueline.87@gmail.com>
2020-03-14 10:08:05 -0400

    upload declare my love

---
+++ script.js
@@ -0,0 +1,27 @@
+function showMelove(){
+    let name="name";
+    prompt ("What's your name sweety?");
+    alert;
+    console.log
+
+ let color=prompt ("What's your favorite color?");
+
+if (color== "Black"){
+ alert("We're Soulmates!");
+}
+if (color== "Purple"){
+alert("Reeeeally!?");
+}
+
+if (color== "Green"){
+ alert("Green is PEOPLE!");
+}
+else if(color!="Black", "Purple", "Green"){
+    alert("We are not compatible. Sorry not sorry -Reeses")
+}
+}
+
+
+
+
+    
